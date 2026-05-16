b60a5a14c481ab90a2d2da10521a8c9d248c7889
Gary Nye <garynye@gmail.com>
2026-05-16 18:50:50 +1000

    Extend hero fade behind journey

--- src/App.jsx
+++ src/App.jsx
@@ -66,7 +66,7 @@ function App() {
                 boxShadow: "none",
                 borderRadius: 0,
                 p: 2,
-                height: isMobile ? window.innerHeight : 0.6 * window.innerHeight,
+                height: isMobile ? window.innerHeight : 0.68 * window.innerHeight,
                 backgroundPosition: isMobile ? "31% 0%" : "0% 0%",
                 backgroundSize: "cover",
                 backgroundRepeat: "no-repeat",
